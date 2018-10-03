a938176885ab363854f217751c6fe5d6091dd00f
Kenytt Avery <kavery@fullerton.edu>
2018-10-02 19:12:30 -0700

    Use extension method for displayList()

--- lang/kotlin/ReliableNumberList.kts
+++ lang/kotlin/ReliableNumberList.kts
@@ -1,9 +1,9 @@
 // See Gaddis, Starting out with C++, Early Objects, Ninth Edition, Chapter 17.
 
-fun displayList(l: List<*>) = l.forEach { print("$it ") }
+fun List<*>.displayList() = this.forEach { print("$it ") }
 
 val list = listOf(2.5, 7.9, 12.6)
-println(list.joinToString(" "))
+list.displayList(); println()
 
 val range = (1..5)
 val squareList = range.map { k -> k * k }
@@ -11,8 +11,8 @@ val cubeList = range.map { it * it * it }
 
 var otherList = squareList
 print("Result of the copy constructor is: ")
-displayList(otherList); println()
+otherList.displayList(); println()
 
 otherList = cubeList
 print("Result of assignment is: ")
-displayList(otherList); println()
+otherList.displayList(); println()
